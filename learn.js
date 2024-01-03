9de42cc5a5a736cc70a62c8fe43ca294fffd3759
Munzer Alajweh <ce.munzer.alajweh@gmail.com>
2024-01-03 14:13:47 +0300

    init vue.js app using vue-cli

---
+++ learn.js
@@ -0,0 +1,43 @@
+/*
+https://git-scm.com/download/win
+or this command :
+$: winget install --id Git.Git -e --source winget
+
+
+
+git hub
+--new Repositories : public - mit license 
+--<>code >> ssh or htttps  copy linke example:
+https://github.com/MunzerAlAjweh/tab-tracker.git
+$: git clone link....git
+$: cd tab-tracker
+
+//*maybe for mac os
+$: ls >>>should give list of folder and files /// not worked
+$: PWD >>> not worked
+$: cat LICENSE >>> not worked
+//*
+
+anyway inside our folder we see only License file
+
+$: npm i -g vue-cli
+now we can use vue command
+
+$: vue command run as admin in cmd , but in terminal of vs code it couldnt
+
+$: vue list >> to get available template
+$: vue init webpack client
+>>e / e /e /e/y/y/standard/y/y/y/?not sure
+
+$: cd client
+$: npm i
+$: npm run dev // run the frontend server
+
+
+$:git status
+$:git add --all
+$:git commit -m 'init vue.js app using vue-cli' // not work on cmd but 
+
+
+
+*/
